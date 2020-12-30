b19a50b300af2a4f52a22d1cf686e5ea1c790bc2
Senpai <senpai@mail.ru>
2020-12-31 00:43:58 +0300

    Facade for Product

---
+++ homework4/ProductController.java
@@ -0,0 +1,56 @@
+package com.geekbrains.geek.market.controllers;
+
+import com.geekbrains.geek.market.dto.PageDto;
+import com.geekbrains.geek.market.dto.ProductDto;
+import com.geekbrains.geek.market.entities.ProductEntity;
+import com.geekbrains.geek.market.exceptions.ResourceNotFoundException;
+import com.geekbrains.geek.market.services.ProductService;
+import com.geekbrains.geek.market.utils.ProductFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping(produces = "application/json")
+    public PageDto getAllProducts(@RequestParam(defaultValue = "1", name = "p") Integer page, @RequestParam Map<String, String> params) {
+        ProductFilter productFilter = new ProductFilter(params);
+        if (page < 1) page = 1;
+        return productService.findAll(productFilter.getSpec(), page - 1, 5);
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ProductEntity getProductById(@PathVariable Long id) {
+        return productService.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("ProductEntity with id=" + id + " doesn't exists"));
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ProductEntity createProduct(@RequestBody ProductEntity productEntity) {
+        productEntity.setId(null);
+        return productService.SaveOrUpdate(productEntity);
+    }
+
+    @PutMapping(consumes = "application/json", produces = "application/json")
+    public ProductEntity updateProduct(@RequestBody ProductEntity productEntity) {
+        return productService.SaveOrUpdate(productEntity);
+    }
+
+    @DeleteMapping
+    public void delProduct() {
+        productService.deleteAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+}
